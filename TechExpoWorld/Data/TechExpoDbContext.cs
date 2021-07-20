d5503e968493d74cf5e980e7c1e8d69f9bf537a6
Boyan Delchev <boyan.delchev@gmail.com>
2021-07-21 01:36:20 +0300

    Cleaned the project

---
+++ TechExpoWorld/Data/TechExpoDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace TechExpoWorld.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class TechExpoDbContext : IdentityDbContext
+    {
+        public TechExpoDbContext(DbContextOptions<TechExpoDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
